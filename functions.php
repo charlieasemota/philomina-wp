73fa288f38c5e1a1ae5041e1e529c14e16f65630
Charlie Asemota <charlie.asemota@wastronauts.com>
2016-01-25 15:51:04 +0000

    Prefixed image size, removed prefix from 3rd party source linked

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ if ( ! isset( $content_width ) ) {
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'title-tag' );
-add_image_size('featured', 800, 800, true );
+add_image_size('philomina_featured', 800, 800, true );
 
 add_action( 'after_setup_theme', 'philomina_theme_setup' );
 function philomina_theme_setup(){
@@ -17,10 +17,10 @@ add_action( 'wp_enqueue_scripts', 'philomina_main_dependancies' );
 function philomina_main_dependancies() {
     wp_enqueue_style( 'philomina-main-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/fonts/font-awesome.css' );
-    wp_enqueue_style( 'philomina-fancybox-css', get_template_directory_uri() . '/assets/js/fancybox/jquery.fancybox.css' );
-    wp_enqueue_script( 'philomina-html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.js' );
+    wp_enqueue_style( 'fancybox-css', get_template_directory_uri() . '/assets/js/fancybox/jquery.fancybox.css' );
+    wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.js' );
     wp_enqueue_script( 'philomina-main-scripts', get_template_directory_uri() . '/assets/js/custom.js', array( 'jquery' ) );
-    wp_enqueue_script( 'philomina-fancybox-js', get_template_directory_uri() . '/assets/js/fancybox/jquery.fancybox.js', array( 'jquery' ) );
+    wp_enqueue_script( 'fancybox-js', get_template_directory_uri() . '/assets/js/fancybox/jquery.fancybox.js', array( 'jquery' ) );
     if ( is_singular() ) {
 		wp_enqueue_script( "comment-reply" );
 	}
@@ -32,7 +32,7 @@ function philomina_editor_styles() {
 }
 
 register_nav_menus( array(
-    'main_menu' => 'Main Menu'
+    'main_menu' => __('Main Menu', 'philomina')
 ) );
 
 function philomina_creditLink() {
@@ -42,7 +42,7 @@ function philomina_creditLink() {
 
 function philomina_featuredBg( $post ) {
 	$featured = false;
-	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) , 'featured' );
+	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) , 'philomina_featured' );
 	$featured = $thumbnail['0'];
 	if( !$featured ) {
 		$featured = get_template_directory_uri() . "/assets/img/no-img.png";
